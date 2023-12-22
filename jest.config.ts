017b22d151c80246741409213d8dd88d171f3379
theluanhoang <hoangtheluan2016@gmail.com>
2023-12-22 21:41:10 +0700

    fix

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/src/$1",
+    },
+    testPathIgnorePatterns: ["/node_modules/", "<rootDir>/dist/"],
+    collectCoverage: true,
+    verbose: true,
+    reporters: ["default", ["jest-junit", { outputDirectory: "reports", outputName: "jest-junit.xml" }]],
+};
+
+export default config;
